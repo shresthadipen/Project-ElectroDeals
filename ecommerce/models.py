b726a6e8285c7725c7ddfa809a0d3fcf44405833
dipen-shrestha <dipenshrestha227@gmail.com>
2025-01-01 16:28:14 +0545

    image field added in category

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length = 50)
-
+    image = models.ImageField(upload_to='upload/category', default='')
     def __str__(self):
         return self.name
     
