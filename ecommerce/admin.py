781e4a8496054fc73e4def321ffac51d59f435f5
dipen-shrestha <dipenshrestha227@gmail.com>
2025-01-01 16:31:47 +0545

    added new table in db 'Brand'

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Brand
 
 class AdminProduct(admin.ModelAdmin):
-    list_display = ['id', 'name', 'price','category','description' ]
+    list_display = ['id', 'name', 'price', 'category', 'brand', 'description']
 
+
+admin.site.register(Brand)
 admin.site.register(Category)
 admin.site.register(Product, AdminProduct)
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='upload/brand', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+
 class Category(models.Model):
     name = models.CharField(max_length = 50)
     image = models.ImageField(upload_to='upload/category', default='')
@@ -12,6 +20,7 @@ class Product(models.Model):
     name = models.CharField(max_length = 50)
     price = models.IntegerField(default = 0)
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length = 200, default = '')
     image = models.ImageField(upload_to='upload/product/')
 
