3ce714acc6ad6fd0ea2f2db820976adc40222c5c
dipen-shrestha <dipenshrestha227@gmail.com>
2025-01-16 18:07:52 +0545

    Inserted brand

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -4,7 +4,10 @@ from .models import Category, Product, Brand
 class AdminProduct(admin.ModelAdmin):
     list_display = ['id', 'name', 'price', 'category', 'brand', 'description']
 
+class AdminBrand(admin.ModelAdmin):
+    list_display = ['id', 'name', 'image']
 
-admin.site.register(Brand)
+
+admin.site.register(Brand, AdminBrand)
 admin.site.register(Category)
 admin.site.register(Product, AdminProduct)
