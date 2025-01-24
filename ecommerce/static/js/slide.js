652e9b210b96d9f630aea2b3a940cccd113795d4
YeetusMaximus <yeetusmaximus911@gmail.com>
2025-01-24 22:09:32 +0545

    slide fix

--- ecommerce/static/js/slide.js
+++ ecommerce/static/js/slide.js
@@ -1,21 +1,26 @@
-let currentIndex = 0;
+const slides = document.querySelectorAll(".brand-container");
+let counter = 0;
 
-    // Get the brands container and its children
-    const brandsContainer = document.querySelector('.brands');
-    const totalBrands = document.querySelectorAll('.brand-container').length;
+slides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`; 
+});
 
-    // Function to move the slider
-    function moveSlider(direction) {
-        // Update the current index based on the direction of the click
-        currentIndex += direction;
-
-        // Prevent the index from going out of bounds
-        if (currentIndex < 0) {
-            currentIndex = 0;
-        } else if (currentIndex >= totalBrands) {  // Adjust this number based on how many brands to show at once
-            currentIndex = totalBrands;
-        }
-
-        // Move the brands container based on the index
-        brandsContainer.style.transform = `translateX(-${currentIndex * 220}px)`; // 220px is the width of each brand
+const goNext = () => {
+    if(counter < slides.length){
+        counter++;
+        slideIndex();
     }
+};
+
+const goPrev = () => {
+    if (counter > 0) { 
+        counter--;
+        slideIndex();
+    }
+};
+
+const slideIndex = () => {
+    slides.forEach((slide) => {
+        slide.style.transform = `translateX(-${counter * 103}%)`; 
+    });
+};
