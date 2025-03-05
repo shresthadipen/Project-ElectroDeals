19def8c3d13ed2a2a82597c847464dcfeb673dd9
dipen-shrestha <dipenshrestha227@gmail.com>
2025-03-05 23:00:28 +0545

    dashboard search feature done

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,29 @@
 from django.urls import path
-from .views import dashboard
+from .views import (dashboard, product_dash,brand_dash,category_dash,customer_dash,message_dash,
+                    dashboard_home,order_dash,setting_dash,
+                    search_products_dash, search_category_dash, 
+                    search_order_dash, search_brand_dash, search_message_dash, 
+                    search_customer_dash, product_detail)
 
 urlpatterns = [
-    path('dashboard/', dashboard, name='dashboard'),
+     path('dashboard/', dashboard_home, name='dashboard'),
+    path('brand_dash/', brand_dash, name='brand_dash'),
+    path('category_dash/', category_dash, name='category_dash'),
+    path('customer_dash/', customer_dash, name='customer_dash'),
+    path('message_dash/', message_dash, name='message_dash'),
+    path('order_dash/', order_dash, name='order_dash'),
+    path('product_dash/', product_dash, name='product_dash'),
+    path('setting_dash/', setting_dash, name='setting_dash'),
+    path('search_product_dash/', search_products_dash, name='search_products_dash'),
+    path('search_order_dash/', search_order_dash, name='search_order_dash'),
+    path('search_category_dash/', search_category_dash, name='search_category_dash'),
+    path('search_brand_dash/', search_brand_dash, name='search_brand_dash'),
+    path('search_customer_dash/', search_customer_dash, name='search_customer_dash'),
+    path('search_message_dash/', search_message_dash, name='search_message_dash'),
+
+
+# details page
+    path('product_detail/<int:product_id>/', product_detail, name='product_detail'),
+
+
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from ecommerce.models import *
+from django.http import JsonResponse
+from django.db.models import Q
+from datetime import datetime
 
 
 # Create your views here.
@@ -10,6 +13,10 @@ def dashboard(request):
     order = Order.get_all_order().order_by("-id")
     customer = Customer.objects.all().order_by("-id")
     message = ContactUs.objects.all().order_by("-id")
+    totalSales = Order.get_total_sales()
+    totalOrders = Order.get_total_orders()
+    totalUsers = Customer.get_total_user()
+
 
     return render(
         request,
@@ -21,6 +28,157 @@ def dashboard(request):
             "orders" : order,
             "customers": customer,
             "messages" : message,
+            "totalSales" : totalSales,
+            "totalOrders" : totalOrders,
+            "totalUsers" : totalUsers,
 
+
+        },
+    )
+
+
+def dashboard_home(request):
+    totalOrder = Order.get_total_orders()
+    totalSales = Order.get_total_sales()
+    totalUser = Customer.get_total_user()
+    order = Order.get_all_order().order_by("-id")
+    message = ContactUs.objects.all().order_by("-id")
+
+    return render(request, "dashboardHome.html", {"totalOrder" : totalOrder, "totalSales" : totalSales, "totalUser" : totalUser, "orders": order, "messages" : message})
+
+def category_dash(request):
+    message = ContactUs.objects.all().order_by("-id")
+
+    category = Category.get_all_category().order_by("-id")
+    return render(request, "category_dash.html", { "category" : category, "messages" : message})
+
+def customer_dash(request):
+    message = ContactUs.objects.all().order_by("-id")
+
+    customer = Customer.objects.all().order_by("-id")
+    return render(request, "customer_dash.html", { "customers" : customer, "messages" : message})
+
+def message_dash(request):
+    message = ContactUs.objects.all().order_by("-id")
+
+    message = ContactUs.objects.all().order_by("-id")
+    return render(request, "message_dash.html", { "messages" : message})
+
+def order_dash(request):
+    message = ContactUs.objects.all().order_by("-id")
+
+    order = Order.get_all_order().order_by("-id")
+    return render(request, "order_dash.html", { "orders" : order, "messages" : message})
+
+def brand_dash(request):
+    message = ContactUs.objects.all().order_by("-id")
+
+    brands = Brand.get_all_brand().order_by("-id")
+    return render(request, "brand_dash.html", { "brand" : brands, "messages" : message})
+
+def product_dash(request):
+    message = ContactUs.objects.all().order_by("-id")
+
+    products = Product.objects.all().order_by("-id")
+    return render(request, "product_dash.html", { "products" : products, "messages" : message})
+
+def setting_dash(request):
+    message = ContactUs.objects.all().order_by("-id")
+
+    return render(request, "setting_dash.html" , {"messages" : message})
+
+
+def search_products_dash(request):
+    query = request.GET.get('query', '')
+    products = Product.objects.filter(name__icontains=query) | Product.objects.filter(brand__name__icontains=query) | Product.objects.filter(category__name__icontains=query)
+    products = products.distinct()  
+    message = ContactUs.objects.all().order_by("-id")
+    
+
+    return render(request, 'search/search_product_dash.html', {
+        'products': products, "messages" : message
+    })
+
+def search_order_dash(request):
+    query = request.GET.get('query', '')
+
+    orders = Order.objects.all()
+    if query.lower() in ['true', 'false']:
+        is_complete = query.lower() == 'true'
+        orders = orders.filter(complete=is_complete)
+    else:
+        orders = orders.filter(
+            Q(transaction_id__icontains=query) |
+            Q(payment_method__icontains=query) | 
+            Q(customer__name__icontains=query) |  
+            Q(customer__user__username__icontains=query) 
+        ).distinct()
+
+    messages = ContactUs.objects.all().order_by("-id")
+
+    return render(request, 'search/search_order_dash.html', {
+        'orders': orders,
+        'messages': messages
+    })
+
+
+def search_category_dash(request):
+    query = request.GET.get('query', '') 
+    if query:
+        categories = Category.objects.filter(name__icontains=query)
+    else:
+        categories = Category.objects.all()  
+    return render(request, 'search/search_category.html', {
+        'category': categories
+    })
+
+
+
+def search_brand_dash(request):
+    query = request.GET.get('query', '')
+    brand = Brand.objects.filter(name__icontains=query) 
+    brand = brand.distinct()  
+    message = ContactUs.objects.all().order_by("-id")
+    
+
+    return render(request, 'search/search_brand.html', {
+        'brand': brand, "messages" : message
+    })
+
+
+def search_message_dash(request):
+    query = request.GET.get('query', '')
+    
+    message = ContactUs.objects.filter(name__icontains=query) | ContactUs.objects.filter(email__icontains=query)
+    message = message.distinct() 
+
+    messages = ContactUs.objects.all().order_by("-id")
+    
+    return render(request, 'search/search_message.html', {
+        'message': message, 
+        'messages': messages  
+    })
+
+def search_customer_dash(request):
+    query = request.GET.get('query', '')
+    customers = Customer.objects.filter(name__icontains=query) | Customer.objects.filter(email__icontains=query) 
+    customers = customers.distinct()  
+    message = ContactUs.objects.all().order_by("-id")
+    
+
+    return render(request, 'search/search_user.html', {
+        'customers': customers, "messages" : message
+    })
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    message = ContactUs.objects.all().order_by("-id")
+
+    return render(
+        request,
+        "details/product_details.html",
+        {
+            "product": product,"messages" : message
         },
     )
